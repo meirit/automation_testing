5348f78eacd7c6435e475a8daefb54b28ec0ad33
meirit <meiritaserdikauskaite@gmail.com>
2023-10-30 19:59:05 +0200

    Add SeleniumEasy Class

---
+++ src/test/java/lt/meirita/dem/SeleniumEasyBasicFirstForm.java
@@ -0,0 +1,57 @@
+package lt.meirita.dem;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SeleniumEasyBasicFirstForm {
+
+    String inputMessage = "Meirita Serdikauskaite";
+    String expectedMessage = "Meirita Serdikauskaite";
+    String actualMessage;
+
+    @Test
+
+    public void testSingleInputField(){
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+
+        WebDriver driver = new ChromeDriver(options);
+
+//        driver.get("chrome://settings/");
+//        ((JavascriptExecutor) driver).executeScript("chrome.settingsPrivate.setDefaultZoom(0.5)");
+
+        driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement inputUserMessage = driver.findElement(By.id("user-message"));
+        inputUserMessage.sendKeys(inputMessage);
+
+        WebElement showMesssage =driver.findElement(By.className("btn-primary"));
+        showMesssage.click();
+
+        WebElement displayMessage = driver.findElement(By.id("display"));
+        actualMessage = displayMessage.getText();
+
+        Assert.assertTrue(
+                actualMessage.contains(expectedMessage),
+                "\nActual: %s, \nExpected: %s".formatted(actualMessage, expectedMessage)
+        );
+
+
+
+
+    }
+
+}
